c4f5b1b76827739c00e8372d55ce73725f61b94b
Serhii_R <rsp89@i.ua>
2023-02-23 00:02:42 +0200

    ADD: Post model

--- MyAwesomeBlog/settings.py
+++ MyAwesomeBlog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 
+import blog.apps
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
